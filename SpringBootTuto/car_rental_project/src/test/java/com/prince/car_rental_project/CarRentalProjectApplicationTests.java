190a8caa82bc6ca9cc805dff366e3a085857405e
Pakodtogo <119876308+Pakodtogo@users.noreply.github.com>
2024-01-22 14:59:45 +0000

    Add files via upload
    bnonjour

---
+++ SpringBootTuto/car_rental_project/src/test/java/com/prince/car_rental_project/CarRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prince.car_rental_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
